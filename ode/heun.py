4c91884523fb7a399a5ab609411c710925c36e3f
Andrês <andresroliveira98@gmail.com>
2024-06-26 14:31:30 -0300

    Create ODE solvers

---
+++ ode/heun.py
@@ -0,0 +1,48 @@
+"""
+    Given x' = f(x), find x(t) for t in [0, T] with x(0) = x0.
+"""
+
+def heun(f, x0, T, n):
+    """
+    Solve x' = f(x), x(0) = x0, with the Heun method.
+
+    Parameters
+    ----------
+    f : function
+        The function f(x) such that x' = f(x).
+    x0 : float
+        The initial condition x(0) = x0.
+    T : float
+        The final time.
+    n : int
+        The number of steps to take.
+
+    Returns
+    -------
+    list of float
+        The solution x(t) at each time t.
+    """
+    x = x0
+    dt = T / n
+    result = [x0]
+    for _ in range(n):
+        k1 = f(x)
+        k2 = f(x + dt * k1)
+        x += dt * (k1 + k2) / 2
+        result.append(x)
+
+    return result
+
+
+def f(x):
+    return (1 - x) * x
+
+def main():
+    x0 = 0.1
+    T = 5
+    n = 100
+    result = heun(f, x0, T, n)
+    print(result)
+
+if __name__ == "__main__":
+    main()
---
+++ ode/runge_kutta.py
@@ -0,0 +1,51 @@
+"""
+    Given x' = f(x), find x(t) for t in [0, T] with x(0) = x0.
+"""
+
+def runge_kutta(f, x0, T, n):
+    """
+    Solve x' = f(x), x(0) = x0, with the Runge-Kutta method.
+
+    Parameters
+    ----------
+    f : function
+        The function f(x) such that x' = f(x).
+    x0 : float
+        The initial condition x(0) = x0.
+    T : float
+        The final time.
+    n : int
+        The number of steps to take.
+
+    Returns
+    -------
+    list of float
+        The solution x(t) at each time t.
+    """
+    x = x0
+    dt = T / n
+    result = [x0]
+    for _ in range(n):
+        k1 = f(x)
+        k2 = f(x + dt * k1 / 2)
+        k3 = f(x + dt * k2 / 2)
+        k4 = f(x + dt * k3)
+        x += dt * (k1 + 2 * k2 + 2 * k3 + k4) / 6
+        result.append(x)
+
+    return result
+
+def f(x):
+    return (1 - x) * x
+
+
+def main():
+    x0 = 0.1
+    T = 5
+    n = 100
+    result = runge_kutta(f, x0, T, n)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
