0a194c102d435afb09680ac13c67ac1cbe34e40d
Andrês <andresroliveira98@gmail.com>
2026-01-30 17:15:40 -0300

    Refactor __all__ exports in integration and interpolation modules for improved readability

--- interpolation/__init__.py
+++ interpolation/__init__.py
@@ -7,4 +7,7 @@ from .newton_divided_diff import newton_divided_diff
 from .spline import linear_spline, cubic_spline_natural
 from .vandermonde import vandermonde
 
-__all__ = ["lagrange", "newton_divided_diff", "linear_spline", "cubic_spline_natural", "vandermonde"]
+__all__ = [
+    "lagrange", "newton_divided_diff", "linear_spline", "cubic_spline_natural",
+    "vandermonde"
+]
